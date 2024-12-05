6e8f2902cc83b48c62dfb853362aed2938938676
Yuheng Wu <wyh_ah@outlook.com>
2024-12-04 19:37:01 -0500

    DDL and DML MySQL

---
+++ concole.sql
@@ -0,0 +1,22 @@
+show databases;
+
+drop database databaseproject;
+create database databaseproject;
+
+select * from databaseproject.WXY_CRUISE;
+select * from databaseproject.WXY_STATEROOM;
+select * from databaseproject.WXY_RESTAURANT;
+select * from databaseproject.WXY_PACKAGE;
+select * from databaseproject.WXY_ENTERTAINMENT;
+select * from databaseproject.WXY_TRIP;
+select * from databaseproject.WXY_TRIP_CRUISE;
+select * from databaseproject.WXY_PORT;
+select * from databaseproject.WXY_TRIP_PORT;
+select * from databaseproject.WXY_INVOICE;
+select * from databaseproject.WXY_GROUP;
+select * from databaseproject.WXY_GROUP_INVOICE;
+select * from databaseproject.WXY_PAYMENT;
+select * from databaseproject.WXY_USER;
+select * from databaseproject.WXY_PASSENGER;
+select * from databaseproject.WXY_PASSENGER_ROOM;
+
