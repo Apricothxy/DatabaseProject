1324c77fbc81eccab8906e62dfed36fd739d2b53
Yuheng Wu <wyh_ah@outlook.com>
2024-12-08 00:29:32 -0500

    correct search stop

--- database/console.sql
+++ database/console.sql
@@ -43,3 +43,29 @@ from wxy_trip_port tp_start
             JOIN wxy_port p_end ON tp_end.port_id = p_end.port_id
             LEFT JOIN wxy_trip_port tp_stop ON tp_start.trip_id = tp_stop.trip_id AND tp_stop.type = 'stop_port'
             LEFT JOIN wxy_port p_stop ON tp_stop.port_id = p_stop.port_id;
+
+INSERT INTO WXY_TRIP (TRIP_ID, NIGHTS_NUM)
+VALUES (2, 6);
+
+INSERT INTO WXY_PORT (PORT_ID, PORT_NAME, STATE, COUNTRY, ADDRESS1, ADDRESS2, NEAREST_AIRPORT_NAME, NUMBER_OF_PARKING_SPOTS)
+VALUES (4, 'Cancun', 'Quintana Roo', 'Mexico', '200 Holiday Blvd', NULL, 'Cancun International Airport', 300);
+
+-- Add a new port for Panama
+INSERT INTO WXY_PORT (PORT_ID, PORT_NAME, STATE, COUNTRY, ADDRESS1, ADDRESS2, NEAREST_AIRPORT_NAME, NUMBER_OF_PARKING_SPOTS)
+VALUES (5, 'Panama City', 'Panama', 'Panama', '300 Harbor Road', NULL, 'Tocumen International Airport', 400);
+
+-- Set the route for trip_id=2:
+-- Start at New York (2), stop at Miami (1), stop at Panama (4), end at Cancun (3)
+INSERT INTO WXY_TRIP_PORT (TRIP_ID, PORT_ID, TYPE, START_DATE, END_DATE)
+VALUES (2, 2, 'start_port', '2024-12-01', '2024-12-01');
+
+INSERT INTO WXY_TRIP_PORT (TRIP_ID, PORT_ID, TYPE, START_DATE, END_DATE)
+VALUES (2, 1, 'stop_port', '2024-12-02', '2024-12-02');
+
+INSERT INTO WXY_TRIP_PORT (TRIP_ID, PORT_ID, TYPE, START_DATE, END_DATE)
+VALUES (2, 5, 'stop_port', '2024-12-04', '2024-12-04');
+
+INSERT INTO WXY_TRIP_PORT (TRIP_ID, PORT_ID, TYPE, START_DATE, END_DATE)
+VALUES (2, 4, 'end_port', '2024-12-07', '2024-12-07');
+
+update wxy_cruise set trip_id = 2 where cruise_name = 'Ocean Explorer';
